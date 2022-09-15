c42ea12822fec6f8af5fa9893c7f7da549f3e109
MaysLastPlay0 <105645266+MaysLastPlay0@users.noreply.github.com>
2022-09-15 12:10:47 +0300

    Fix

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2328,6 +2328,7 @@ class PlayState extends MusicBeatState
 		} else {
 			scoreTxt.text = 'Score: ' + songScore + ' | Combo Breaks: ' + songMisses + ' | Health: ' + Math.round(health * 50) + '%' + ' | Accuracy: ' + Highscore.floorDecimal(ratingPercent * 100, 2) + '%' + ' | ' + '[' + ratingFC + '] ';
 		}
+
 		if(botplayTxt.visible) {
 			botplaySine += 180 * elapsed;
 			botplayTxt.alpha = 1 - Math.sin((Math.PI * botplaySine) / 180);
