3818253b047cf2b9ac9c82a6eb35e8e49e39ebf4
MaysLastPlay0 <105645266+MaysLastPlay0@users.noreply.github.com>
2022-09-18 10:17:52 +0300

    Final

--- source/FunkinLua.hx
+++ source/FunkinLua.hx
@@ -136,7 +136,7 @@ class FunkinLua {
 		set('rating', 0);
 		set('ratingName', '');
 		set('ratingFC', '');
-		set('version', MainMenuState.psychEngineVersion.trim());
+		set('version', MainMenuState.playEngineVersion.trim());
 		
 		set('inGameOver', false);
 		set('mustHitSection', false);
