25f4b2dcadec6b1e6d26e044009a437eeb240585
MaysLastPlay <114417870+MaysLastPlay@users.noreply.github.com>
2022-10-29 15:09:13 +0300

    Funkin Rework

--- source/options/OptimizationSubState.hx
+++ source/options/OptimizationSubState.hx
@@ -29,7 +29,7 @@ import openfl.Lib;
 
 using StringTools;
 
-class GraphicsSettingsSubState extends BaseOptionsMenu
+class OptimizationSubState extends BaseOptionsMenu
 {
 	public function new()
 	{
