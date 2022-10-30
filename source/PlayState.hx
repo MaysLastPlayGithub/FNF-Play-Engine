911d44b003708d242ff3c32a14b95e2d6dc57ef3
MaysLastPlay <114417870+MaysLastPlay@users.noreply.github.com>
2022-10-30 10:01:52 +0200

    Fuuuuck

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -888,7 +888,7 @@ class PlayState extends MusicBeatState
 
 		if(ClientPrefs.timeBarType == 'Timebar Visible')
 		{
-			timeTxt.text = SONG.song + ' [' + FlxStringUtil.formatTime(Math.floor((FlxG.sound.music.length - (Conductor.songPosition - ClientPrefs.noteOffset)) / 1000), false) + ']';
+			timeTxt.text = SONG.song + ' [' + ${FlxStringUtil.formatTime(secondsTotal, false)} '/' ${FlxStringUtil.formatTime(Math.floor(songLength / 1000), false)} + ']';
 		}
 		updateTime = showTime;
 
