20875a0452bfdc7cf831b5d1ee540f48e28af59d
MaysLastPlay <114417870+MaysLastPlay@users.noreply.github.com>
2022-10-30 10:47:06 +0200

    Freedom.

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -888,7 +888,7 @@ class PlayState extends MusicBeatState
 
 		if(ClientPrefs.timeBarType == 'Timebar Visible')
 		{
-			timeTxt.text = SONG.song + ' [' + FlxStringUtil.formatTime(Math.floor(songLength / 1000), false), + ']';
+			timeTxt.text = SONG.song + ' [' + storyDifficultyText + ']';
 		}
 		updateTime = showTime;
 
